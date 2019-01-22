0fb0720e5856485fa417b49c0e4ed9f0ef4ce15b
Igor Bykov <con29rus@live.ru>
2019-01-22 19:07:52 +0300

    add missing interfaces and update AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersEditorComponent } from './users-editor/users-editor.component';
+import { UserEditorComponent } from './user-editor/user-editor.component';
+import { CountriesEditorComponent } from './countries-editor/countries-editor.component';
+import { CountryEditorComponent } from './country-editor/country-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersEditorComponent,
+    UserEditorComponent,
+    CountriesEditorComponent,
+    CountryEditorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/interfaces/user-coutry.interface.ts
@@ -0,0 +1,7 @@
+export interface UserCountryInterface {
+  id: number;
+  userId: number;
+  countriyId: number;
+  visited: boolean;
+  hasVisa: boolean;
+}
