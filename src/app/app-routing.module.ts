60172a6bd5a4e26e47db6f42e18a4d2f8cc4b8f3
Igor Bykov <con29rus@live.ru>
2019-01-22 19:05:07 +0300

    update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersEditorComponent } from './users-editor/users-editor.component';
+import { CountriesEditorComponent } from './countries-editor/countries-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'editor/users', component: UsersEditorComponent },
+  { path: 'editor/coutries', component: CountriesEditorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
